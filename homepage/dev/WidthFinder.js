21ac10c0d069d9c3c1cd5acc462c96c545822943
Daniel <82470542+ruqzuq@users.noreply.github.com>
2023-01-17 23:21:23 +0100

    chore: update WidthFinder

--- homepage/dev/WidthFinder.js
+++ homepage/dev/WidthFinder.js
@@ -2,14 +2,33 @@ import React, { useEffect, useState } from 'react';
 import { SvgFont } from '../../package/src/text/SvgFont';
 
 function renderRect(props = {}) {
-  const { width = 10, height = 10, x = 0, y = 0 } = props;
-  return `<rect x="${x}" y="${y}" width="${width}" height="${height}" style="fill:rgb(200,88,88)" />`;
+  const { width = 10, height = 10, x = 0, y = 0, fill } = props;
+  return `<rect x="${x}" y="${y}" width="${width}" height="${height}" style="fill:${
+    fill ?? 'rgb(200,88,88)'
+  }" />`;
 }
 
 function renderFinder(char, width) {
+  // Only required to set TextDimension statics.
+  const allChars =
+    '+ !&quot;%23$%&amp;\'()*+,-./0123456789:;&lt;=&gt;?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopqrstuvwxyz{|}~';
+
+  const newChar =
+    char === '"'
+      ? '&quot;'
+      : char === '#'
+      ? '%23'
+      : char === '&'
+      ? '&amp;'
+      : char === '<'
+      ? '&lt;'
+      : char === '>'
+      ? '&gt;'
+      : char;
+
   return `
-	${renderRect({ x: 7.22, y: 0, height: 18, width })}
-	<text x="0" y="14.67" font-family="Svg" font-weight="400" font-size="14px" letter-spacing="0">_${char}_</text>
+	${renderRect({ x: 6.3, y: 0, height: 16, width })}
+	<text x="0" y="12.7" font-family="Svg" font-weight="400" font-size="14px" letter-spacing="0">_${newChar}_</text>
 	`;
 }
 
@@ -18,10 +37,12 @@ function renderFinder(char, width) {
  */
 export function WidthFinder() {
   const [width, setWidth] = useState(10);
-
   const [image, setImage] = useState('');
 
-  const char = 'A';
+  const [output, setOutput] = useState('');
+
+  // Only required to set TextDimension statics.
+  const [currentChar, setCurrentChar] = useState(32);
 
   /**
    * Click/focus div before key.
@@ -44,7 +65,17 @@ export function WidthFinder() {
         setWidth((width) => width + 0.1);
         break;
       case 32:
-        console.log('// ' + char + '\n' + width + ', \n');
+        // eslint-disable-next-line no-case-declarations
+        const newOutput =
+          output +
+          '// ' +
+          String.fromCharCode(currentChar) +
+          '\n' +
+          Math.round(width * 1000) +
+          ', \n';
+        console.log(newOutput);
+        setOutput(newOutput);
+        setCurrentChar(currentChar + 1);
         break;
     }
   };
@@ -52,11 +83,11 @@ export function WidthFinder() {
   useEffect(() => {
     setImage(
       `<svg xmlns="http://www.w3.org/2000/svg" version="1.2" ><defs>${SvgFont}</defs>${renderFinder(
-        char,
+        String.fromCharCode(currentChar),
         width
       )}</svg>`
     );
-  }, [width]);
+  }, [width, currentChar]);
 
   return (
     <div onKeyDown={handleKey} tabIndex="0" style={{ position: 'absolute' }}>
