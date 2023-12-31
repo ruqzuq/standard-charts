714d45b136ea1af25ab56bc576f6df552fed4a0e
Daniel <82470542+ruqzuq@users.noreply.github.com>
2023-12-31 19:43:38 +0100

    feat: introduce VarianceColumnChart

--- src/charts/VarianceColumnChart.ts
+++ src/charts/VarianceColumnChart.ts
@@ -1,7 +1,7 @@
 import { ChartTypes } from '..';
 import { Box } from '../base/Box';
 import { Color } from '../base/Color';
-import { extractSimpleValue } from '../base/Data';
+import { Scenario, extractSimpleValue } from '../base/Data';
 import { SimpleDataType } from '../base/DataTypes';
 import { MaxMeasure } from '../base/MaxMeasure';
 import { Rect } from '../base/Rect';
@@ -12,6 +12,7 @@ import { Constants } from './Constants';
 export interface VarianceColumnChartProps extends ChartProps<SimpleDataType> {
   chartType: ChartTypes.VarianceColumn;
   variance: 'ABSOLUTE' | 'RELATIVE';
+  axis?: Scenario;
 }
 
 export class VarianceColumnChart extends Chart<SimpleDataType> {
@@ -20,11 +21,15 @@ export class VarianceColumnChart extends Chart<SimpleDataType> {
   columnWidth: number;
   columnMargin: number;
   axisElementWidth: number;
+  secondaryLeftOffset: number = 0;
+  axis: Scenario = Scenario.AC;
 
   fontSize: number;
 
   constructor(props: VarianceColumnChartProps) {
     super(props);
+
+    const { axis } = props;
 
     this.fontSize = 8;
     [10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32].forEach((fontSize) => {
@@ -36,11 +41,13 @@ export class VarianceColumnChart extends Chart<SimpleDataType> {
     this.axisElementWidth =
       (this.width - 2 * Constants.ChartPadding) / this.data.length;
 
-    this.columnWidth =
-      (Constants.ColumnMarginToWidthRelation * this.axisElementWidth) /
-      (Constants.ColumnMarginToWidthRelation + 2);
+    this.columnWidth = Constants.ColumnToWidthRelation * this.axisElementWidth;
     this.columnMargin =
-      this.columnWidth / Constants.ColumnMarginToWidthRelation;
+      Constants.ColumnMarginToWidthRelation * this.axisElementWidth;
+
+    if (axis) {
+      this.axis = axis;
+    }
   }
 
   fontTooBig(fontSize: number) {
@@ -57,28 +64,28 @@ export class VarianceColumnChart extends Chart<SimpleDataType> {
       }
     });
 
-    const columnWidth = maxTextWidth.max;
+    const elementWidth = maxTextWidth.max / Constants.ColumnToWidthRelation;
 
     return (
-      this.data.length *
-        (columnWidth +
-          2 * (columnWidth / Constants.ColumnMarginToWidthRelation)) >
-      this.width - 2 * Constants.ChartPadding
+      this.data.length * elementWidth > this.width - 2 * Constants.ChartPadding
     );
   }
 
-  scaleTooBig(scale: number) {
+  scaleToHeight(scale: number) {
     const negativeMax = new MaxMeasure();
     const positiveMax = new MaxMeasure();
 
     this.data.forEach((dataPoint) => {
       const { value } = extractSimpleValue(dataPoint);
 
-      const valueText = new Text(this.context, value, {
-        size: this.fontSize,
-      });
-
       if (value) {
+        const valueText = new Text(
+          this.context,
+          (value > 0 ? '+' : '') + value,
+          {
+            size: this.fontSize,
+          }
+        );
         if (value >= 0) {
           positiveMax.addEntry(scale * value, valueText.boxHeight);
         } else {
@@ -88,16 +95,18 @@ export class VarianceColumnChart extends Chart<SimpleDataType> {
     });
 
     // annahme, dass scaling präziser wird
-    this.axisOffset = this.height - Constants.ChartPadding - negativeMax.max;
+    this.axisOffset = Constants.ChartPadding + positiveMax.max;
 
-    return (
-      negativeMax.max + positiveMax.max >
-      this.height - 2 * Constants.ChartPadding
-    );
+    return negativeMax.max + positiveMax.max + 2 * Constants.ChartPadding;
+  }
+
+  scaleTooBig(scale: number) {
+    return this.scaleToHeight(scale) > this.height;
   }
 
   draw(scale: number) {
     this.drawDebug();
+    this.drawAxis();
 
     this.data.forEach((dataPoint, index) => {
       const { value, scenario } = extractSimpleValue(dataPoint);
@@ -107,6 +116,7 @@ export class VarianceColumnChart extends Chart<SimpleDataType> {
           x:
             Constants.ChartPadding +
             (this.columnMargin + this.columnWidth / 2) +
+            this.secondaryLeftOffset +
             +index * this.axisElementWidth,
           y: this.axisOffset,
         },
@@ -115,9 +125,13 @@ export class VarianceColumnChart extends Chart<SimpleDataType> {
       );
 
       if (value) {
-        const valueText = new Text(this.context, value, {
-          size: this.fontSize,
-        });
+        const valueText = new Text(
+          this.context,
+          (value > 0 ? '+' : '') + value,
+          {
+            size: this.fontSize,
+          }
+        );
 
         let valueBox;
         if (value >= 0) {
@@ -140,14 +154,43 @@ export class VarianceColumnChart extends Chart<SimpleDataType> {
         valueText.draw(this.context, this.debug);
       }
     });
+  }
 
-    // Axis
-    this.context.fillStyle = '#494949';
-    this.context.fillRect(
-      Constants.ChartPadding,
-      this.axisOffset - 1,
-      this.width - 2 * Constants.ChartPadding,
-      2
-    );
+  drawAxis() {
+    if (this.axis === Scenario.PY) {
+      // Axis
+      this.context.fillStyle = Color.Fill.PY;
+      this.context.fillRect(
+        Constants.ChartPadding,
+        this.axisOffset - 2,
+        this.width - 2 * Constants.ChartPadding,
+        4
+      );
+    } else if (this.axis === Scenario.FC) {
+    } else if (this.axis === Scenario.PL) {
+      // Axis
+      this.context.fillStyle = Color.Stroke.PL;
+      this.context.fillRect(
+        Constants.ChartPadding,
+        this.axisOffset - 2,
+        this.width - 2 * Constants.ChartPadding,
+        1
+      );
+      this.context.fillRect(
+        Constants.ChartPadding,
+        this.axisOffset + 1,
+        this.width - 2 * Constants.ChartPadding,
+        1
+      );
+    } else {
+      // Axis
+      this.context.fillStyle = '#494949';
+      this.context.fillRect(
+        Constants.ChartPadding,
+        this.axisOffset - 1,
+        this.width - 2 * Constants.ChartPadding,
+        2
+      );
+    }
   }
 }
