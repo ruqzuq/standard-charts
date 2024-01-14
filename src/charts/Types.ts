edae3a40722b908e2fbd718e7b8968ea5d752637
Daniel <82470542+ruqzuq@users.noreply.github.com>
2024-01-14 22:04:20 +0100

    feat: introduce improved types for charts

---
+++ src/charts/Types.ts
@@ -0,0 +1,13 @@
+export enum ChartType {
+  Scenario = 'SCENARIO',
+  Stacked = 'STACKED',
+  Variance = 'VARIANCE',
+  Waterfall = 'WATERFALL',
+}
+
+export enum ChartStyle {
+  Column = 'COLUMN',
+  Bar = 'BAR',
+  Line = 'LINE',
+  Area = 'AREA',
+}
